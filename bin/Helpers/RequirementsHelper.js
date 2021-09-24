e20e80f345a563270027467cbfd13a0caec560eb
Pixote <guiihpr@gmail.com>
2021-09-24 17:38:49 -0300

    added new depedencies to run create-sandbox

--- bin/Helpers/RequirementsHelper.js
+++ bin/Helpers/RequirementsHelper.js
@@ -4,6 +4,9 @@ const fs = require('fs')
 const path = require('path')
 
 class RequirementsHelper {
+  constructor (commandName) {
+    this.commandName = commandName
+  }
   async checkAllRequirements(configPath,  relativePath = '') {
     if (!this.configFileExists(configPath, relativePath)) throw new Error(`${path.join(relativePath, configPath)} doesnt exists`)
 
@@ -15,12 +18,34 @@ class RequirementsHelper {
     await this.allCommandsExists(requiredDepedencies)
 
     const configFile = JSON.parse(fs.readFileSync(path.join(relativePath, configPath)))
-    if (Array.isArray(configFile.projects) && configFile.projects.length == 0) throw new Error(`There's no projects into "projects" props`)
-
-    for (const project of configFile.projects) {
-      if (!project.envfile) throw `Project ${project.repo} doesnt have an .env file`
+    const rulesToValidate = {
+      init: () => {
+        this.projectShouldExists(configFile)
+      },
+      'create-sandbox': () => {
+        this.sandboxPropShouldExists(configFile)
+      }
     }
+    const validateFunction = rulesToValidate[this.commandName]
+    if (validateFunction) validateFunction(configFile)
     return configFile
+  }
+
+  projectShouldExists(configFile) {
+    if (Array.isArray(configFile.projects) && configFile.projects.length == 0) throw new Error(`There's no projects into "projects" props`)
+    for (const project of configFile.projects) {
+      if (!project.envfile) throw new Error(`Project ${project.repo} doesnt have an .env file`)
+    }
+    return true
+  }
+
+  sandboxPropShouldExists(configFile) {
+    if (!configFile.sandbox) throw new Error(`prop sandbox is required`)
+    if (!configFile.sandbox.name) throw new Error(`prop sandbox.name is required`)
+    if (!configFile.sandbox.kubernetes) throw new Error(`prop sandbox.kubernetes is required`)
+    if (!configFile.sandbox.kubernetes.reference_namespaces) throw new Error(`prop sandbox.kubernetes.reference_namespaces is required`)
+    if (!configFile.sandbox.kubernetes.reference_namespaces.length) throw new Error(`prop sandbox.kubernetes should have at least one`)
+    return true
   }
 
   configFileExists(configPath, relativePath = null) {
